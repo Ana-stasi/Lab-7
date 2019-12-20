0c6920ab1b44f58d9769af6504288d9d06e4cc0a
Ana-stasi <anastasia.lapa@gmail.com>
2019-12-20 10:11:35 +0200

    тест123

--- client/main.js
+++ client/main.js
@@ -74,10 +74,6 @@ socket.on("system new", name => {
 socket.on("render message", data => {
   if(e.target.elements[0].value == "кот"){
     alert("кот");
-    document.getElementById("messages").innerText += `[${data.username}]: ${data.message} \n`; 
   }
-  else{
-    document.getElementById("messages").innerText += `[${data.username}]: ${data.message} \n`; 
-  }
-
+  document.getElementById("messages").innerText += `[${data.username}]: ${data.message} \n`; 
 });
